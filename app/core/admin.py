69ead207b9e751448d55e48a55dc7a2d1beb8dbb
Saddem Khadra <saddem@saveyourwardrobe.com>
2022-07-01 13:18:04 +0100

    django test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(User, UserAdmin)
--- app/core/tests.py
+++ app/core/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
 class ModelTests(TestCase):
@@ -33,3 +34,25 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="sad@sad.com",
+            password="saddem123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com',
+            password='saddem123',
+            name="TEST"
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
