1196b743292057f04eae80829035aae276109b7c
Saddem Khadra <saddem@saveyourwardrobe.com>
2022-07-02 11:39:20 +0100

    Update admin to support cutom user model

--- app/core/tests.py
+++ app/core/tests.py
@@ -56,3 +56,15 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
